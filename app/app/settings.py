d4820b5a01413274b4366c4c90d85eb8c6cc73ae
mutibAkbar <mutibakbar2017@gmail.com>
2022-09-08 11:57:30 +0500

    Swagger and openapi added

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'           #Settinng Auth_User_Model configuration
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
